5d68347ac07145c5912f62228dbc0ed532e19b10
Ersultan <example@gmail.com>
2023-02-06 12:47:59 +0300

    192. Добавим React в проект

--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore, bindActionCreators } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import { Provider } from "react-redux";
+
+import App from "./components/App";
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById("counter").textContent = getState().value;
-};
-
-subscribe(update);
-
-// const bindActionCreator =
-//   (creator, dispatch) =>
-//   (...args) => {
-//     dispatch(creator(...args));
-//   };
-
-const { incDispatch, decDispatch, rndDispatch } = bindActionCreators(
-  actions,
-  dispatch
-);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", incDispatch);
-
-document.getElementById("dec").addEventListener("click", decDispatch);
-
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
